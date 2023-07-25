147ceb591875d505bea2822497924b6421becc39
Barzat <ivy.mike555@gmail.com>
2023-07-25 17:01:45 +0600

    homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,7 @@ import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import MoreIcon from "@mui/icons-material/MoreVert";
 import MapIcon from "@mui/icons-material/Map";
+import LocalAtmIcon from "@mui/icons-material/LocalAtm";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,106 +1,28 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Button from "@mui/material/Button";
-import CameraIcon from "@mui/icons-material/PhotoCamera";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import Link from "@mui/material/Link";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React from "react";
+import video from "../assets/video.mp4";
+import { Button } from "@mui/material";
 
-function Copyright() {
+const HomePage = () => {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
-// const cards = [1, 2, 3];
-
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
-
-export default function Album() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-
-      <main>
-        {/* Hero unit */}
-
-        <Box
+    <div className="home">
+      <div className="overplay"></div>
+      <video src={video} autoPlay loop muted />
+      <div className="content">
+        <h1>City Map</h1>
+        {/* <p>Welcome</p> */}
+        <Button
+          variant="contained"
           sx={{
-            bgcolor: "background.paper",
-            pt: 8,
-            pb: 6,
+            margin: "20px",
+            backgroundColor: "black",
+            borderRadius: "100px 10px 100px 10px",
           }}
         >
-          <Box>
-            <video src="https://www.youtube.com/watch?v=MbZoDTcfvaw"></video>
-          </Box>
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              City Map
-            </Typography>
-            <Typography
-              variant="h5"
-              align="center"
-              color="text.secondary"
-              paragraph
-            >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-              quisquam dolores cum nemo dolorem sapiente, ipsum qui ducimus
-              nobis. Tempore hic excepturi eaque ut nesciunt blanditiis, optio
-              laboriosam beatae accusamus.
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Start</Button>
-              <Button variant="outlined">Ne Start</Button>
-            </Stack>
-          </Container>
-        </Box>
-      </main>
-      {/* Footer */}
-      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </Box>
-      {/* End footer */}
-    </ThemeProvider>
+          Welcome
+        </Button>
+      </div>
+    </div>
   );
-}
+};
+
+export default HomePage;
